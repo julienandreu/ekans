46fe541c69e0c4d2a6a1e55b719697f0536af02e
Julien Andreu <julienandreu@me.com>
2024-06-22 23:42:12 -0400

    feat: Install and setup raw FastAPI

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from middlewares.request_id import with_request_id
-
-app = Flask(__name__)
-
-app.wsgi_app = with_request_id(app.wsgi_app)
-
-import routes.root
--- routes/root.py
+++
@@ -1,6 +0,0 @@
-from flask import jsonify
-from app import app
-
-@app.get("/")
-def root():
-  return jsonify({"🐍":"Ekans"})
